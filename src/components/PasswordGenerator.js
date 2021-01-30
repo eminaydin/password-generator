fb1fdc2035f47000aea8a5b9b5a95921fa005aa7
Emin <eminaydin93@gmx.de>
2021-01-30 22:55:24 +0100

    Add password logic

--- src/components/PasswordGenerator.js
+++ src/components/PasswordGenerator.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import {
   Button,
+  Card,
+  CardContent,
   Checkbox,
   Container,
+  FormControl,
   FormControlLabel,
   FormGroup,
   Slider,
@@ -10,55 +13,92 @@ import {
 } from "@material-ui/core";
 
 export default function PasswordGenerator() {
-  const [hasSpecialChars, setHasSpecialChars] = useState(false);
-  const [hasCamelCase, setHasCamelCase] = useState(false);
+  const [password, setPassword] = useState("");
+  const [hasSpecialChars, setHasSpecialChars] = useState(true);
+  const [hasCapitals, setHasCapitals] = useState(false);
+  const [passwordLength, setPasswordLength] = useState(12);
+
+  const generatePassword = () => {
+    const upperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    const lowerCase = upperCase.toLowerCase();
+    const specialCharacters = `1234567890!"#%^&'()*+,-/;<=>?@[]\\_\`{}|~`;
+    let possiblePassword = "";
+    if (hasCapitals) possiblePassword += upperCase;
+    if (lowerCase) possiblePassword += lowerCase;
+    if (hasSpecialChars) possiblePassword += specialCharacters;
+    let generatedPassword = "";
+    for (let i = 0; i < passwordLength; i++) {
+      generatedPassword +=
+        possiblePassword[
+          Math.floor(Math.random() * Math.floor(possiblePassword.length - 1))
+        ];
+    }
+    setPassword(generatedPassword);
+  };
 
   return (
-    <Container maxWidth="sm">
-      <Container>
-        <h1>Generate Password</h1>
-        <TextField
-          disabled
-          id="standard-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-        />
-      </Container>
-      <Button variant="contained" color="primary">
-        Generate
-      </Button>
-      <h3>Password Length</h3>
-      <Slider
-        defaultValue={12}
-        aria-labelledby="discrete-slider-small-steps"
-        step={1}
-        marks
-        max={45}
-        valueLabelDisplay="auto"
-      />
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={hasSpecialChars}
-              onChange={() => setHasSpecialChars(!hasSpecialChars)}
-              name="hasSpecialChars"
+    <Card maxWidth="sm" className="generatorWrapper">
+      <CardContent>
+        <Container className="generatorHeader">
+          <h1>Generate Password</h1>
+          <FormControl fullWidth>
+            <TextField
+              disabled
+              id="standard-disabled"
+              label="Password"
+              value={password}
             />
-          }
-          label="Special Characters"
+          </FormControl>
+        </Container>
+        <Button
+          variant="contained"
+          color="primary"
+          className="generateButton"
+          onClick={generatePassword}
+        >
+          Generate
+        </Button>
+        <h3>Password Length</h3>
+        <Slider
+          value={passwordLength}
+          aria-labelledby="discrete-slider-small-steps"
+          step={1}
+          marks
+          min={12}
+          max={36}
+          valueLabelDisplay="auto"
+          onChange={(event, newValue) => {
+            setPasswordLength(newValue);
+            newValue !== passwordLength && generatePassword();
+          }}
+          color="primary"
         />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={hasCamelCase}
-              onChange={() => setHasCamelCase(!hasCamelCase)}
-              name="hasCamelCase"
-              color="primary"
-            />
-          }
-          label="Camel Case"
-        />
-      </FormGroup>
-    </Container>
+
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={hasSpecialChars}
+                onChange={() => setHasSpecialChars(!hasSpecialChars)}
+                name="hasSpecialChars"
+                color="primary"
+              />
+            }
+            label="Special Characters"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={hasCapitals}
+                onChange={() => setHasCapitals(!hasCapitals)}
+                name="hasCapitals"
+                color="primary"
+              />
+            }
+            label="Capital Letters"
+          />
+        </FormGroup>
+      </CardContent>
+    </Card>
   );
 }
